e715cfc00d5fcbd2e2e26c147c66fe73c31fb865
korvio1 <flybart24@gmail.com>
2025-04-24 21:55:20 +0200

    1

---
+++ Projekt/item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Item
+{
+    public string Name { get; }
+    public decimal WeightKg { get; }
+    public int WeirdnessLevel { get; }
+    public bool IsFragile { get; }
+    public Item()
+	{
+
+	}
+}
