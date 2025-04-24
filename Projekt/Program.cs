308b2c3ab41cf6f061fb4e20aa7b9a1d5255fa96
korvio1 <flybart24@gmail.com>
2025-04-24 23:06:06 +0200

    7

--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter warehouse capacity: ");
+        int capacity = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter max total weight (kg): ");
+        double maxWeight = double.Parse(Console.ReadLine());
+
+        Warehouse warehouse = new Warehouse(capacity, maxWeight);
+
+
+    }
